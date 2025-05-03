ad4627ee31114ac868f218feced044bb3d113639
Gabriel Henrique Silva e Ferreira <gabrielhsf01@gmail.com>
2025-05-03 00:38:59 -0300

    docs: atualiza README com changelog e instruções

--- BibliotecaSimples/src/Livro.java
+++ BibliotecaSimples/src/Livro.java
@@ -1,18 +1,25 @@
 public class Livro {
-	private String titulo;
-	private String autor;
+    private String isbn;
+    private String titulo;
+    private String autor;
 
-	public Livro(String titulo, String autor) {
-		this.titulo = titulo;
-		this.autor = autor;
-	}
+    
+    public Livro(String isbn, String titulo, String autor) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
 
-	public String getTitulo() {
-		return titulo;
-	}
+    
+    public String getIsbn() {
+        return isbn;
+    }
 
-	public String getAutor() {
-		return autor;
+    public String getTitulo() {
+        return titulo;
+    }
 
-	}
+    public String getAutor() {
+        return autor;
+    }
 }
